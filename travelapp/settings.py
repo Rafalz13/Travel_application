d52aff3eb4395de9fbf93aa5e8efdebb71995670
Rafalz13 <rafal.ziemianek@student.up.krakow.pl>
2020-07-17 17:54:17 +0200

    staticfiles added

--- travelapp/settings.py
+++ travelapp/settings.py
@@ -126,12 +126,13 @@ USE_TZ = True
 
 #URL danych folderow w projekcie
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS= [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
